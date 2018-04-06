031bd54f448859674e59c1dbf60398443f6b7865
Chris <chris@shyft.network>
2018-04-05 20:15:34 -0400

    [ updated to enable test for submapindex > 1 in stress tests ]

--- hugeGanacheStart.sh
+++ hugeGanacheStart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ganache-cli -a=240 -e=500 -d -m="salmon armor trial million lizard castle until flush good proud fog later" --gasLimit 10000000
+ganache-cli -a=260 -e=500 -d -m="salmon armor trial million lizard castle until flush good proud fog later" --gasLimit 10000000
 
 
