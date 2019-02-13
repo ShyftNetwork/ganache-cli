c33c479dcb0faaa5f42f889279e3cce93bbebb87
Chris <chris@shyft.network>
2019-02-12 19:01:43 -0500

    [ updates to start script ]

--- hugeGanacheStart.sh
+++ hugeGanacheStart.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-ganache-cli -a=260 -e=500 -d -m="salmon armor trial million lizard castle until flush good proud fog later" --gasLimit 10000000
-
-
+ganache-cli -a=500 -e=200000000000000000000 -m="salmon armor trial million lizard castle until flush good proud fog later" --gasLimit 10000000
